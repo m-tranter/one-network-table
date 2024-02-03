3728821b8b6d5280600fb2e75732a9b1fde4ae98
antiloquax <tranter.m@sky.com>
2024-02-03 12:12:40 +0000

    Refactor

--- app/index.js
+++ app/index.js
@@ -3,13 +3,12 @@ import fetch from 'node-fetch';
 import path from 'path';
 import bodyParser from 'body-parser';
 import convert from 'xml-js';
-
 import { fileURLToPath } from 'url';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const dir = path.join(__dirname, '../public');
 const port = process.env.PORT || 3001;
-
 const url =
   'https://datacloud.one.network/?app_key=94db72b2-058e-2caf-94de16536c81';
 const user = 'cheshireeast';
@@ -23,14 +22,12 @@ app.use(express.static(dir));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Remove some unnecessary duplication.
 const dedup = (arr) => {
   return arr.reduce((acc, e) => {
     e.forEach((l) => {
       l = l.replace(', Cheshire East', '');
-      if (l.split(' ').length === 1 && acc.some((e) => e.includes(l))) {
-        return acc;
-      }
-      if (l.length && !acc.includes(l) && !l.includes('Ward')) {
+      if (!acc.includes(l)) {
         acc.push(l);
       }
     });
@@ -38,6 +35,21 @@ const dedup = (arr) => {
   }, []);
 };
 
+// Ignore description items that are a single word.
+const dedupDesc = (arr) => {
+  return arr.reduce((acc, e) => {
+    if (e.split(' ').length === 1) {
+      return acc;
+    }
+    return [...acc, e];
+  }, []);
+};
+
+const initialCap = (str) => {
+  return `${str[0].toUpperCase()}${str.slice(1)}`;
+};
+
+// Constructor for main 'situation' object.
 const Item = function (obj) {
   let rec = obj.situation.situationRecord;
   if (Array.isArray(rec)) {
@@ -50,25 +62,23 @@ const Item = function (obj) {
   }
 };
 
-const cap = (str) => {
-  return `${str[0].toUpperCase()}${str.slice(1)}`;
-};
-
+// Separate constructor fot the details part of the object.
 const Details = function (obj) {
   let sev = obj.severity;
+  this.severity = sev ? initialCap(sev._text) : '';
   this.id = obj.situationRecordCreationReference
     ? obj.situationRecordCreationReference._text
     : '';
-  this.severity = sev ? cap(sev._text) : '';
   this.startDate =
     obj.validity.validityTimeSpecification.overallStartTime._text;
   this.endDate = obj.validity.validityTimeSpecification.overallEndTime._text;
   let comment = obj.generalPublicComment.comment.values.value;
   if (Array.isArray(comment)) {
-    this.description = comment.map((e) => cap(e._text)).join('^#');
+    this.description = dedupDesc(comment.map((e) => initialCap(e._text))).join(
+      '^#'
+    );
   } else {
-    this.description = cap(comment._text);
-    console.log(this.description);
+    this.description = initialCap(comment._text);
   }
   this.impact = obj.impact.delays.delaysType._text;
   this.url = obj.urlLink.urlLinkAddress._text;
@@ -84,35 +94,33 @@ const Details = function (obj) {
   this.worksState = state ? state.description._text : '';
 };
 
+// Helper function to get location information.
 const loc = function (obj) {
-  try {
-    return obj.groupOfLocations.tpegPointLocation.point.name.reduce(
-      (acc, e) => {
-        let temp = e.descriptor.values.value._text.trim();
-        if (temp === 'Cheshire East') {
-          return acc;
-        }
-        if (acc[0] && temp.startsWith(acc[0])) {
-          return [temp];
-        }
-        return [...acc, temp];
-      },
-      []
-    );
-  } catch (err) {
-    let temp = obj.groupOfLocations.locationContainedInItinerary;
-    try {
-      return [
-        temp[0].location.tpegPointLocation.point.name[0].descriptor.values.value
-          ._text,
-      ];
-    } catch (err) {
-      return ['None'];
-    }
+  let tpeg = obj.groupOfLocations.tpegPointLocation;
+  if (tpeg && tpeg.name) {
+    return tpeg.point.name.reduce((acc, e) => {
+      let temp = e.descriptor.values.value._text.trim();
+      if (temp === 'Cheshire East' || temp.includes('Ward')) {
+        return acc;
+      }
+      if (acc[0] && temp.startsWith(acc[0])) {
+        return [temp];
+      }
+      return [...acc, temp];
+    }, []);
   }
+  let itinerary = obj.groupOfLocations.locationContainedInItinerary;
+  if (itinerary) {
+    return [
+      itinerary[0].location.tpegPointLocation.point.name[0].descriptor.values
+        .value._text,
+    ];
+  }
+  return ['None'];
 };
 
-app.get('/*', (req, res) => {
+// Route
+app.get('/*', (_, res) => {
   fetch(url, {
     headers: {
       Authorization: 'Basic ' + btoa(`${user}:${password}`),
@@ -123,16 +131,19 @@ app.get('/*', (req, res) => {
       return response.text();
     })
     .then((text) => {
+      // Get the timestamp of the data.
       let date = text
         .split(/\n\s*\n/)[0]
         .split('<publicationTime>')[1]
         .split('</publicationTime>')[0];
+      // Get the situations from the XML.
       let works = text.split(/\n\s*\n/).slice(1);
       let last = works.pop().split(/\n/).slice(0, -3);
       works.push(last);
       let temp = works.reduce((acc, sit) => {
         let v = JSON.parse(convert.xml2json(sit, { compact: true, spaces: 4 }));
         let item = new Item(v);
+        // Ignore duplicates or situations with no location info.
         let el = acc.find((e) => e.id === item.id);
         if (!el && item.locations !== 'None') {
           acc.push(item);
